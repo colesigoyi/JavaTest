02f5912d0bd20cc05c37ea4d2ea74de44dd27140
taoxuefeng <tao642133424@163.com>
2019-06-09 22:19:14 +0800

    多态

---
+++ src/test/Test0609_1.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-09 22:18
+ * @ desc: 多态
+ **/
+
+public class Test0609_1 {
+    public static void main(String[] args) {
+
+    }
+}
