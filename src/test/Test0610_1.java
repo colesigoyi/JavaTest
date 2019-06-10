cae1660a150f9a70b2877ea0911e83def8a44095
taoxuefeng <tao642133424@163.com>
2019-06-10 23:47:51 +0800

    instanceof关键字

---
+++ src/test/Test0610_1.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-10 23:46
+ * @ desc: instanceof关键字
+ **/
+
+public class Test0610_1 {
+    public static void main(String[] args) {
+
+    }
+}
