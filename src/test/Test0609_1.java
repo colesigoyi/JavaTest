06bc6dd6ca19489f2e6b861819a1c168f5153b6a
taoxuefeng <tao642133424@163.com>
2019-06-10 23:24:43 +0800

    多态

--- src/test/Test0609_1.java
+++ src/test/Test0609_1.java
@@ -9,6 +9,46 @@ package test;
 
 public class Test0609_1 {
     public static void main(String[] args) {
+        HomeChicken hc = new HomeChicken("小鸡鸡");
+        hc.eat();
 
+        YeChicken yc = new YeChicken("大鸡鸡");
+        yc.eat();
+    }
+}
+abstract class Chicken1{
+    private String name;
+    public Chicken1(){}
+
+    public Chicken1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void eat();
+}
+
+class HomeChicken extends Chicken1{
+    public HomeChicken(String name){
+        super(name);
+    }
+    public void eat(){
+        System.out.println(this.getName() + ",我爱吃米");
+    }
+}
+
+class YeChicken extends Chicken1{
+    public YeChicken(String name){
+        super(name);
+    }
+    public void eat(){
+        System.out.println(this.getName() + "，我爱吃虫子");
     }
 }
