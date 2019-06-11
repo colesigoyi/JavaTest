858ed2f5bd093408ae058a13ab57a6ee0c20d580
taoxuefeng <tao642133424@163.com>
2019-06-12 01:44:26 +0800

    设计模式：策略模式

---
+++ src/test/Test0612_2.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-12 01:43
+ * @ desc: 接口应用-策略模式
+ **/
+
+public class Test0612_2 {
+    public static void main(String[] args) {
+
+    }
+}
