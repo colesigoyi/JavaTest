52a7c9d456ebc25924ff90a74ebc4e041a2ea628
taoxuefeng <tao642133424@163.com>
2019-06-12 01:30:27 +0800

    设计模式

--- src/test/Test0612_1.java
+++ src/test/Test0612_1.java
@@ -1,5 +1,6 @@
 package test;
 
+
 /**
  * @ program: java_study
  * @ author:  TaoXueFeng
@@ -11,6 +12,33 @@ package test;
 
 public class Test0612_1 {
     public static void main(String[] args) {
+        Usermanager um = new Usermanager();
+        um.action("admin","add");
 
     }
+}
+abstract class BaseManager{
+    public void action(String name, String method){
+        if("admin".equals(name)){
+            execute(method);
+        }else {
+            System.out.println("你没有操作权限，请联系管理员。");
+        }
+    }
+    public abstract void execute(String method);
+}
+
+class Usermanager extends BaseManager {
+    //用户是否登录的验证
+    //验证成功后执行某个操作
+    public void execute(String method){
+        if("add".equals(method)){
+            System.out.println("执行了添加操作");
+        }else if("del".equals(method)){
+            System.out.println("执行了删除操作");
+        }
+    }
+}
+class Classmanager{
+
 }
