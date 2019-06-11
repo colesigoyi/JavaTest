6f4efe567519a2dbecc67017dedcd52be9b71ae3
taoxuefeng <tao642133424@163.com>
2019-06-12 01:19:00 +0800

    设计模式

---
+++ src/test/Test0612_1.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-12 01:12
+ * @ desc: 抽象类应用--模板方法模式
+ *          定义一个操作中的算法的骨架，而将一些可变部分的实现延迟到子类中，
+ *          模板方法模式使得子类可以不改变一个算法的结构即可重新定义概算发的某些特定的步骤
+ **/
+
+public class Test0612_1 {
+    public static void main(String[] args) {
+
+    }
+}
