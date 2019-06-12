691b367e432a42386469ceca824e146034c16d93
taoxuefeng <tao642133424@163.com>
2019-06-12 19:02:51 +0800

    代理模式

---
+++ src/test/Test0612_5.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-12 19:01
+ * @ desc: 代理模式
+ **/
+
+public class Test0612_5 {
+    public static void main(String[] args) {
+
+    }
+}
