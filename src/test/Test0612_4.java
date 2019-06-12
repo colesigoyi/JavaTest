10ac545ecaad11a8648b2592bc9688cb49601314
taoxuefeng <tao642133424@163.com>
2019-06-12 18:28:15 +0800

    简单工厂模式

---
+++ src/test/Test0612_4.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-12 18:27
+ * @ desc: 简单工厂模式
+ **/
+
+public class Test0612_4 {
+    public static void main(String[] args) {
+
+    }
+}
