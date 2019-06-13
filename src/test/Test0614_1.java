497c0af58c2ec31a749d5e5c7621df8af904d58c
taoxuefeng <tao642133424@163.com>
2019-06-14 01:59:41 +0800

    链表

---
+++ src/test/Test0614_1.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-14 01:58
+ * @ desc: 链表：一种常见的基础数据结构，是一种线性表，但并不会按照线性的顺序存储
+ *          而是在每一个节点里存到下一个节点的指针（pointer）
+ **/
+
+public class Test0614_1 {
+    public static void main(String[] args) {
+
+    }
+}
