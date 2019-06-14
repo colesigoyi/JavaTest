44c27077ef7507acd83cfd807b075386c68e9577
taoxuefeng <tao642133424@163.com>
2019-06-15 01:33:36 +0800

    链表

--- src/test/Test0614_1.java
+++ src/test/Test0614_1.java
@@ -13,3 +13,45 @@ public class Test0614_1 {
 
     }
 }
+
+class NodeManager{
+    private Node root;//根节点
+
+
+    private class Node{
+        private int data;
+        private Node next;//把当前类型作为属性
+        public Node(int data){
+            this.data = data;
+        }
+        public void setData(int data){
+            this.data = data;
+        }
+        public int getData(){
+            return data;
+        }
+        //添加节点
+        public void addNode(int data){
+
+        }
+        //删除节点
+        public void delNode(int data){
+
+        }
+        //输出所有节点
+        public void printNode(){
+
+        }
+        public boolean findNode(int data){
+            return false;
+        }
+        //更新节点
+        public void updateNode(int oldData, int newData){
+
+        }
+        //插入数据
+        public void insert(int index, int data){
+
+        }
+    }
+}
