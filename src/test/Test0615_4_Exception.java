d3ad48c94e78c1f50f92f7e2f045818e2bce5055
taoxuefeng <tao642133424@163.com>
2019-06-15 17:35:21 +0800

    异常处理throw

--- src/test/Test0615_4_Exception.java
+++ src/test/Test0615_4_Exception.java
@@ -9,6 +9,23 @@ package test;
 
 public class Test0615_4_Exception {
     public static void main(String[] args) {
-
+        try {
+            div(20,0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("结束");//用try使得后面的代码可以执行
+        //div(20, 0);
+    }
+    //throws带s在方法的声明上使用，throw与throws配合一起使用
+    private static int div(int a, int b) throws ArithmeticException{
+        try {
+            int c = a/b;
+            return c;
+        }catch (ArithmeticException e){
+            throw new ArithmeticException("除数不能为0");//当一个方法执行到throw，跳出，后面不执行
+        }finally {
+            System.out.println("运行结束");
+        }
     }
 }
