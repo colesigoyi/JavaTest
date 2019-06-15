eed0b506f7d13349029c73859d7ff9a425791a7e
taoxuefeng <tao642133424@163.com>
2019-06-15 17:15:48 +0800

    异常处理throw

---
+++ src/test/Test0615_4_Exception.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-15 17:13
+ * @ desc: 异常处理
+ **/
+
+public class Test0615_4_Exception {
+    public static void main(String[] args) {
+
+    }
+}
