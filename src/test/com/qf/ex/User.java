db1f1d8e6f85ceef6d32e7cabb4209d9b9c5353e
taoxuefeng <tao642133424@163.com>
2019-06-15 19:12:54 +0800

    测试异常

---
+++ src/test/com/qf/ex/User.java
@@ -0,0 +1,68 @@
+package test.com.qf.ex;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-15 18:06
+ * @ desc: 用户
+ **/
+
+public class User {
+    private String username;
+    private String password;
+    private int age;
+    private String sex;
+
+    public User() {
+        super();
+    }
+
+    public User(String username, String password, int age, String sex) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
---
+++ src/test/com/qf/ex/UserService.java
@@ -0,0 +1,19 @@
+package test.com.qf.ex;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-15 18:04
+ * @ desc: 用户类
+ **/
+
+public class UserService {
+    public User Login(String username, String password)throws MyException{
+        if(!"admin".equals(username)){
+            throw new MyException("用户名错误！");
+        }else if (!"12345".equals(password)){
+            throw new MyException("密码错误！");
+        }
+        return new User("admin","12345", 27, "男");
+    }
+}
