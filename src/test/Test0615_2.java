6d2c93b1a5b2837f684e487e863158a12ce6f98a
taoxuefeng <tao642133424@163.com>
2019-06-15 13:23:42 +0800

    包与访问修饰符

---
+++ src/test/Test0615_2.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-15 13:23
+ * @ desc: 包与访问修饰符
+ **/
+
+public class Test0615_2 {
+    public static void main(String[] args) {
+
+    }
+}
