2c68b69885bc1e67bd3b5832755b5d3c5015dd06
taoxuefeng <tao642133424@163.com>
2019-06-16 22:29:54 +0800

    String字符串

--- src/test/com/qf/API/StringDemo.java
+++ src/test/com/qf/API/StringDemo.java
@@ -9,6 +9,11 @@ package test.com.qf.API;
 
 public class StringDemo {
     public static void main(String[] args) {
-
+        //String两种赋值方式
+        //1、直接赋值(推荐使用第一种方式)
+        String s1 = "String";
+        //2、使用new关键字创建对象，new表示申请内存空间
+        String s2 = new String("String");
+        System.out.println(s1==s2);//false
     }
 }
