4dbc0a2b385e1d62f2cfa3e48f51cecff0fd399d
taoxuefeng <tao642133424@163.com>
2019-06-16 22:32:07 +0800

    String字符串

--- src/test/com/qf/API/StringDemo.java
+++ src/test/com/qf/API/StringDemo.java
@@ -11,9 +11,12 @@ public class StringDemo {
     public static void main(String[] args) {
         //String两种赋值方式
         //1、直接赋值(推荐使用第一种方式)
-        String s1 = "String";
+        String s1 = "String";//在常量池创建
         //2、使用new关键字创建对象，new表示申请内存空间
-        String s2 = new String("String");
+
+        //面试题：以下代码创建了几个对象？2个对象
+        String s2 = new String("String");//在堆中创建
+
         System.out.println(s1==s2);//false
     }
 }
