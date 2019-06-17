30a8a9cf84825fa5268d8f156fccddb0858be4f0
taoxuefeng <tao642133424@163.com>
2019-06-17 14:34:23 +0800

    String字符串常用方法

--- src/test/com/qf/API/Test0616_String.java
+++ src/test/com/qf/API/Test0616_String.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 public class Test0616_String {
     public static void main(String[] args) {
-        String str = "abcdefg";
+        String str = " abcdefgabc 12312345 ";
         char c = str.charAt(1);
         System.out.println(c);//b
 
@@ -23,7 +23,30 @@ public class Test0616_String {
         String s2 = new String(cs, 0, 1);
         System.out.println(s2);
 
-        System.out.println(Arrays.toString(str.getBytes()));//[97, 98, 99, 100, 101, 102, 103]
+        System.out.println(Arrays.toString(str.getBytes()));//[97, 98, 99, 100, 101, 102, 103, 97, 98, 99, 49, 50, 51, 49, 50, 51, 52, 53]
 
+        System.out.println(str.replace('b','B'));
+
+        System.out.println(str.replaceAll("\\d","*"));
+
+        System.out.println(str.substring(0,4));//截取
+
+        System.out.println(Arrays.toString(str.split("d",2)));
+
+        System.out.println(str.contains("2"));//是否包含某字符串
+
+        System.out.println(str.indexOf("f"));//从头查找是否存在，显示第一个位置
+
+        System.out.println(str.lastIndexOf("g"));//从后往前找
+
+        System.out.println(str.isEmpty());//是否为空
+
+        System.out.println(str.length());//长度
+
+        System.out.println(str.trim());//去掉开头和结尾的空格
+
+        System.out.println(str.concat("****"));//与字符串拼接
+
+        System.out.println(str.valueOf(true));//转换成字符串
     }
 }
