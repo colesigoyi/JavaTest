baf185a4e3ad50be14a7fd683dbb5f0c4f0cc12b
taoxuefeng <tao642133424@163.com>
2019-06-17 19:16:27 +0800

    Random类

---
+++ src/test/com/qf/MathAndRandom/RandomDemo.java
@@ -0,0 +1,19 @@
+package test.com.qf.MathAndRandom;
+
+import java.util.Random;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-17 18:45
+ * @ desc: Random类
+ **/
+
+public class RandomDemo {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextLong());
+        System.out.println(r.nextInt(10));//10以内的随机数
+
+    }
+}
