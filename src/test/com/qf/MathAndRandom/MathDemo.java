2a7a72166d8611c52e95103563a50e6f98761402
taoxuefeng <tao642133424@163.com>
2019-06-17 18:10:29 +0800

    Math类

---
+++ src/test/com/qf/MathAndRandom/MathDemo.java
@@ -0,0 +1,14 @@
+package test.com.qf.MathAndRandom;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-17 18:09
+ * @ desc: math类
+ **/
+
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+    }
+}
