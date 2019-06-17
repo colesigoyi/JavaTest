ff27be2f6f6b866583d7c02f29cd76ba1377f0aa
taoxuefeng <tao642133424@163.com>
2019-06-17 19:16:50 +0800

    对象比较器

---
+++ src/test/com/qf/Comparable/ComparableDemo.java
@@ -0,0 +1,14 @@
+package test.com.qf.Comparable;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-17 19:15
+ * @ desc: 对象比较器
+ **/
+
+public class ComparableDemo {
+    public static void main(String[] args) {
+
+    }
+}
