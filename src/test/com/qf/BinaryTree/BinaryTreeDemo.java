73fc9fea583299d1b669eca8cc3613d974383ca5
taoxuefeng <tao642133424@163.com>
2019-06-18 16:59:54 +0800

    二叉树

--- src/test/com/qf/BinaryTree/BinaryTreeDemo.java
+++ src/test/com/qf/BinaryTree/BinaryTreeDemo.java
@@ -9,6 +9,17 @@ package test.com.qf.BinaryTree;
 
 public class BinaryTreeDemo {
     public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.add(8);
+        bt.add(3);
+        bt.add(10);
+        bt.add(1);
+        bt.add(6);
+        bt.add(14);
+        bt.add(4);
+        bt.add(7);
+        bt.add(13);
 
+        bt.print();
     }
 }
