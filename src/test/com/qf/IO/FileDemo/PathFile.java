827fcff3ad95d246795d887406541db84833cda7
taoxuefeng <tao642133424@163.com>
2019-06-19 22:50:59 +0800

    工具类

---
+++ src/test/com/qf/IO/FileDemo/PathFile.java
@@ -0,0 +1,16 @@
+package test.com.qf.IO.FileDemo;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-19 19:51
+ * @ desc: 文件夹路径
+ **/
+
+public class PathFile {
+    public static String getPath(){
+        String path = null;
+        return path = "/Users/taoxuefeng/Documents/02_学习文件/" +
+                "01_Java/java/java_study/src/test/com/qf/IO/FileDemo/";
+    }
+}
