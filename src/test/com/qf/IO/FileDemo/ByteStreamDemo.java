4e914546c5f7c63a9482360ab1a11239095fef9b
taoxuefeng <tao642133424@163.com>
2019-06-19 18:00:42 +0800

    字节流

--- src/test/com/qf/IO/FileDemo/ByteStreamDemo.java
+++ src/test/com/qf/IO/FileDemo/ByteStreamDemo.java
@@ -18,7 +18,7 @@ public class ByteStreamDemo {
         in();
     }
     private static void out(){
-        String path = "/Users/***/Documents/02_学习文件/" +
+        String path = "/Users/taoxuefeng/Documents/02_学习文件/" +
                 "01_Java/java/java_study/src/test/com/qf/IO/FileDemo/";
         //0.确定文件
         File file = new File(path + "newTest.txt");
@@ -28,7 +28,7 @@ public class ByteStreamDemo {
 
             OutputStream out = new FileOutputStream(file, true);//append为true表示追加
             //2.要输出的内容
-            String info = "测试代码\r\n";//表示换行
+            String info = "写入测试\r\n";//表示换行
             //String line = System.getProperty("line.separator");//获取换行符
             //3.把内容写入盗文就爱你
             out.write(info.getBytes());
@@ -57,7 +57,7 @@ public class ByteStreamDemo {
             //把数据读到数组中，并返回读取到字节数，当不等于-1时，表示读取到数据，等于-1表示文件已经读完
             while ((len = in.read(bytes)) != -1){
                 //根据读取到的字节数，在转换成字符串内容，添加到StringBuffer中
-                buf.append(new String(bytes));
+                buf.append(new String(bytes,0,len));
             }
             //打印内容
             System.out.println("--------开始复制--------");
