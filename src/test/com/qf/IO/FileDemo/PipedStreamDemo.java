d61a19219e20f9845528f158dafd3e89eadb37d4
taoxuefeng <tao642133424@163.com>
2019-06-24 02:14:42 +0800

    管道流

---
+++ src/test/com/qf/IO/FileDemo/PipedStreamDemo.java
@@ -0,0 +1,16 @@
+package test.com.qf.IO.FileDemo;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-06-24 02:12
+ * @ desc: 管道流
+ **/
+
+public class PipedStreamDemo {
+    public static void main(String[] args) {
+
+    }
+
+
+}
