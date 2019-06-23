45162c83cf001148989eb567c6a4a40fad42241c
taoxuefeng <tao642133424@163.com>
2019-06-24 03:01:33 +0800

    管道流

--- src/test/com/qf/IO/FileDemo/PipedStreamDemo.java
+++ src/test/com/qf/IO/FileDemo/PipedStreamDemo.java
@@ -4,7 +4,8 @@ package test.com.qf.IO.FileDemo;
  * @ program: java_study
  * @ author:  TaoXueFeng
  * @ create: 2019-06-24 02:12
- * @ desc: 管道流
+ * @ desc: 管道流:一个线程写入，一个线程读取
+ *              作用：用于线程之间的数据通讯
  **/
 
 public class PipedStreamDemo {
