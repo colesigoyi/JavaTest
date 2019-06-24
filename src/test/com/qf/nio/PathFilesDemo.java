1c4c8983951020944f4c93d6cf034488fdf56a87
taoxuefeng <tao642133424@163.com>
2019-06-24 22:55:59 +0800

    NIO：Path、Files

--- src/test/com/qf/nio/PathFilesDemo.java
+++ src/test/com/qf/nio/PathFilesDemo.java
@@ -26,6 +26,7 @@ public class PathFilesDemo {
         Path p3 = FileSystems.getDefault().getPath(path.getPath(),"computer.jpg");
 
         //Files类
+        //写入文件
         Path p4 = Paths.get(path.getPath() + "test.txt");
         String info = "输入测试";
         try {
@@ -40,5 +41,45 @@ public class PathFilesDemo {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //复制文件
+        try {
+            Files.copy(p3,Paths.get(path.getPath() + "/test/computer3.jpg"),
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //移动文件
+        try {
+            Files.move(p3,Paths.get(path.getPath() + "/test/computer.jpg"),
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //删除文件
+        try {
+            Files.delete(p3);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //创建新目录，出了最后一个部件，其他的必须已经存在
+        try {
+            Files.createDirectory(Paths.get(path.getPath() + "creatFile"));
+            //Files.createDirectories(path);//创建多级不存在的目录
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //创建文件
+        try {
+            Files.createFile(Paths.get(path.getPath() + "creat.txt"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //添加前缀后缀创建临时文件或临时目录
+        //Path newPath = Files.createTempDirectory(dir, prefix, suffix);
+        //Path newPath = Files.createTempFile(dir, prefix, suffix);
     }
 }
