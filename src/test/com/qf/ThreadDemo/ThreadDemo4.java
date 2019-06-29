df4dee7667679e7c12fcb8726f2b6fa31367ef46
taoxuefeng <tao642133424@163.com>
2019-06-29 13:22:32 +0800

    Thread锁

--- src/test/com/qf/ThreadDemo/ThreadDemo4.java
+++ src/test/com/qf/ThreadDemo/ThreadDemo4.java
@@ -9,6 +9,12 @@ import java.util.concurrent.locks.ReentrantLock;
  * @ desc: 线程同步:
  *          1.多线程共享数据时会发生线程不安全的情况
  *          2.多线程共享数据必须使用同步
+ *          3.实现同步的三种方法
+ *              （1）使用同步代码块
+ *              （2）使用同步方法
+ *              （3）使用Lock（更加灵活）
+ *          多线程共享数据，会有安全问题，使用同步可以解决安全问题，但同时也会牺牲性能
+ *          所以同步的代码块尽量保持简短，把不随数据变化的相关代码移出同步块，不要阻塞
  **/
 
 public class ThreadDemo4 {
@@ -64,15 +70,18 @@ class MyRunable5 implements Runnable{
     //lock实现同步,更加灵活
     private void method2(){
         lock.lock();//锁
-        ticket--;
-        if(ticket>=0){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            ticket--;
+            if(ticket>=0){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() +"--您购买的票剩余:" + ticket + "张票");
             }
-            System.out.println(Thread.currentThread().getName() +"--您购买的票剩余:" + ticket + "张票");
+        } finally {
+            lock.unlock();//释放锁
         }
-        lock.unlock();//释放锁
     }
 }
