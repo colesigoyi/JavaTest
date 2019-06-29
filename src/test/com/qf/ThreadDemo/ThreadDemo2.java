f677f725100e68326873029836a459400bd2cd65
taoxuefeng <tao642133424@163.com>
2019-06-29 09:36:34 +0800

    Thread

--- src/test/com/qf/ThreadDemo/ThreadDemo2.java
+++ src/test/com/qf/ThreadDemo/ThreadDemo2.java
@@ -4,38 +4,72 @@ package test.com.qf.ThreadDemo;
  * @ program: java_study
  * @ author:  TaoXueFeng
  * @ create: 2019-06-28 23:37
- * @ desc:
+ * @ desc:join方法：加入线程，让调用的线程先执行指定时间或执行完毕
+ *          中断线程：
+ *              （1）、使用interrupt方法来中断线程，设置一个中断状态（标记）
+ *              （2）、使用自定义标记(推荐使用）
  **/
 
 public class ThreadDemo2 {
     public static void main(String[] args) {
-        MyRunnable2 mr2 = new MyRunnable2();
+        MyRunable2 mr2 = new MyRunable2();
         Thread t = new Thread(mr2);
-        t.start();
+        //t.start();
+
+        MyRunable3 mr3 = new MyRunable3();
+        Thread t2 = new Thread(mr3);
+        t2.start();
 
         for (int i=0; i<50; i++){
             System.out.println(Thread.currentThread().getName() + "--" + i);
             try {
-                Thread.sleep(300);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             if (i==20){
-                try {
-                    t.join();//让t线程执行完毕
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                //try {
+                //    t.join();//让t线程执行完毕
+                //} catch (InterruptedException e) {
+                //    e.printStackTrace();
+                //}
+                //t.interrupt();//中断线程,只是做了一个中断标记
+                mr3.flag = false;
             }
         }
 
     }
 }
-class MyRunnable2 implements Runnable{
+class MyRunable2 implements Runnable{
     @Override
     public void run() {
         for(int i=0; i<50; i++){
+            if(Thread.interrupted()){//测试中断状态，此方法会把中断标记清除
+                //...其他操作，比如保存
+                break;
+            }
             System.out.println(Thread.currentThread().getName() + "--" + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+class MyRunable3 implements Runnable{
+    public boolean flag = true;
+
+    public MyRunable3() {
+        flag = true;
+    }
+
+    @Override
+    public void run() {
+        int i=0;
+        while (flag){
+            System.out.println(Thread.currentThread().getName() + "==" + i);
             try {
                 Thread.sleep(300);
             } catch (InterruptedException e) {
