31a0a0465f59df54c46a4cd89ee08381450672ae
taoxuefeng <tao642133424@163.com>
2019-07-01 10:54:43 +0800

    枚举

---
+++ src/test/com/qf/EnumDemo/EnumDemo.java
@@ -0,0 +1,14 @@
+package test.com.qf.EnumDemo;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-07-01 10:54
+ * @ desc: 枚举
+ **/
+
+public class EnumDemo {
+    public static void main(String[] args) {
+
+    }
+}
