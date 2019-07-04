2deb5bd4dd506fb939b8c0ad3df1e06202ebb474
taoxuefeng <tao642133424@163.com>
2019-07-04 23:32:43 +0800

    注解

---
+++ src/test/com/qf/AnnotationDemo/AnnotationDemo.java
@@ -0,0 +1,14 @@
+package test.com.qf.AnnotationDemo;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-07-04 23:32
+ * @ desc: 注解
+ **/
+
+public class AnnotationDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/com/qf/AnnotationDemo/Cat.java
@@ -0,0 +1,14 @@
+package test.com.qf.AnnotationDemo;
+
+/**
+ * @ program: java_study
+ * @ author:  TaoXueFeng
+ * @ create: 2019-07-04 23:32
+ * @ desc:
+ **/
+
+public class Cat {
+    public static void main(String[] args) {
+
+    }
+}
