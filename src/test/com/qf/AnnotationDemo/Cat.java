9bec8a40c38d17377f4f2d1215c86125753fa396
taoxuefeng <tao642133424@163.com>
2019-07-04 23:35:31 +0800

    注解

--- src/test/com/qf/AnnotationDemo/Cat.java
+++ src/test/com/qf/AnnotationDemo/Cat.java
@@ -8,7 +8,38 @@ package test.com.qf.AnnotationDemo;
  **/
 
 public class Cat {
-    public static void main(String[] args) {
+    private String name;
+    private int age;
 
+    public Cat() {
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
